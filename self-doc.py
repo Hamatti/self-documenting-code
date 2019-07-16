d1aa980ae2bea3e7d0e010df88a6cd947242087c
Juha-Matti Santala <juhamattisantala@gmail.com>
2019-07-16 17:46:36 +0300

    Refactor the function to be less fragile

--- self-doc.py
+++ self-doc.py
@@ -6,8 +6,10 @@ import re
 
 FUNCTION_DECLARATION_PATTERN = r'^def .*\(.*\):$'
 
-def document(function_name, docstring):
-    with open(__file__, 'r') as source_file:
+def document(docstring):
+    function_name = inspect.stack()[1][3]
+    filename = inspect.stack()[1][1]
+    with open(filename, 'r') as source_file:
         source_code = source_file.read().split('\n')
 
     definitions = [line for line in source_code if re.match(FUNCTION_DECLARATION_PATTERN, line)]
@@ -22,13 +24,14 @@ def document(function_name, docstring):
     if source_code[index].startswith('    """'):
         return False
 
-    new_source.insert(index, docstring)
+    new_source.insert(index, f'    {docstring}')
 
-    with open(__file__, 'w') as source_file:
+    with open(filename, 'w') as source_file:
         source_file.write('\n'.join(new_source))
 
 def self_documenting_sum(a, b):
-    document(inspect.stack()[0][3], '    """Sums up integers."""')
+    """Sums up integers."""
+    document('"""Sums up integers."""')
     return a + b
 
 
