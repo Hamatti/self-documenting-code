fb7238b272f0791d76a5cf8e9d9b49427d468360
Juha-Matti Santala <juhamattisantala@gmail.com>
2019-07-16 17:51:35 +0300

    Refactor selfdocumenter into its own module

--- self-doc.py
+++ self-doc.py
@@ -1,33 +1,7 @@
 # This is a funny little stab at the self-documenting code trope.
 # @author Juha-Matti Santala / hamatti.org
 
-import inspect
-import re
-
-FUNCTION_DECLARATION_PATTERN = r'^def .*\(.*\):$'
-
-def document(docstring):
-    function_name = inspect.stack()[1][3]
-    filename = inspect.stack()[1][1]
-    with open(filename, 'r') as source_file:
-        source_code = source_file.read().split('\n')
-
-    definitions = [line for line in source_code if re.match(FUNCTION_DECLARATION_PATTERN, line)]
-    definition = [definition for definition in definitions if function_name in definition]
-    if not definition:
-        return False
-
-    definition = definition[0]
-    index = source_code.index(definition) + 1
-
-    new_source = source_code[:]
-    if source_code[index].startswith('    """'):
-        return False
-
-    new_source.insert(index, f'    {docstring}')
-
-    with open(filename, 'w') as source_file:
-        source_file.write('\n'.join(new_source))
+from selfdocumenter import document
 
 def self_documenting_sum(a, b):
     """Sums up integers."""
